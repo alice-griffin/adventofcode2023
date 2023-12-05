4ce36c6c0a118dc5d89b2647ec6649b4b0711894
alice-griffin <alicekaygriffin@gmail.com>
2023-12-05 12:46:33 -0500

    first commit

---
+++ Day2/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] games = File.ReadLines("../../TextFile1.txt").ToArray();
+
+            int redTotal = 12;
+            int greenTotal = 13;
+            int blueTotal = 14;
+            List<int> gameIds = new List<int>();
+
+            int totalPwr = 0;
+            for (int i = 0; i < games.Length; i++)
+            {
+                string game = games[i];
+                bool validGame = true;
+
+                int startIndex = game.IndexOf(" ");
+                int endIndex = game.IndexOf(":");
+                string gameId = game.Substring(5, endIndex - startIndex - 1);
+
+                string gameResults = game.Substring(endIndex + 1, game.Length - endIndex - 1);
+
+                var colorResults = gameResults.Split(';');
+
+                int highestRed = 0;
+                int highestBlue = 0;
+                int highestGreen = 0;
+                int numR = 0;
+                int numB = 0;
+                int numG = 0;
+                int power = 0;
+
+                foreach (var color in colorResults)
+                {
+                    var colors = color.Split(',');
+
+                    foreach(var c in colors)
+                    {
+                        if (c.Contains("red"))
+                        {
+                            numR = Convert.ToInt32(new string(c.Where(x => char.IsNumber(x)).ToArray()));
+                            if (numR > highestRed)
+                            {
+                                highestRed = numR;
+                            }
+
+
+                            if (numR > 12)
+                            {
+                                validGame = false;
+                            }
+                        }
+                        if (c.Contains("blue"))
+                        {
+                            numB = Convert.ToInt32(new string(c.Where(x => char.IsNumber(x)).ToArray()));
+                            if (numB > highestBlue)
+                            {
+                                highestBlue = numB;
+                            }
+                            if (Convert.ToInt32(numB) > 14)
+                            {
+                                validGame = false;
+                            }
+
+                        } 
+                        if (c.Contains("green"))
+                        {
+                            numG = Convert.ToInt32(new string(c.Where(x => char.IsNumber(x)).ToArray()));
+                            if (numG > highestGreen)
+                            {
+                                highestGreen = numG;
+                            }
+
+                        }
+                    }
+                }
+
+                power = (highestRed * highestBlue * highestGreen);
+                totalPwr += power;
+
+                if (validGame)
+                {
+                    gameIds.Add(Convert.ToInt32(gameId));
+                }
+
+
+            }
+            Console.WriteLine(totalPwr);
+            int total = gameIds.Sum();
+
+        }
+    }
+}
---
+++ adventofcode2023/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adventofcode2023
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] lines = File.ReadLines("../../TextFile1.txt").ToArray();
+
+            int total = 0;
+            double firstNum = 0;
+            double secondNum = 0;
+
+            var numberTuple = new List<(string, int)>
+            {
+                ("one", 1),
+                ("two", 2),
+                ("three", 3),
+                ("four", 4),
+                ("five", 5),
+                ("six", 6),
+                ("seven", 7),
+                ("eight", 8),
+                ("nine", 9),
+            };
+
+            for (int i = 0; i < lines.Count(); i++)
+            {
+                var line = lines[i];
+
+                Dictionary<int, double> numsAndIndexes = new Dictionary<int, double>();
+
+                foreach(var tuple in numberTuple)
+                {
+                    if (line.Contains(tuple.Item1)) {
+                        List<int> foundIndexes = new List<int>();
+                        int index = line.IndexOf(tuple.Item1, StringComparison.OrdinalIgnoreCase);
+                        while (index != -1)
+                        {
+                            foundIndexes.Add(index);
+                            index = line.IndexOf(tuple.Item1, index + 1, StringComparison.OrdinalIgnoreCase);
+                        }
+                        foreach(var ind in foundIndexes)
+                        {
+                            numsAndIndexes.Add(ind, tuple.Item2);
+                        }
+                    }
+                }
+
+                for(int j = 0; j < line.Length; j++)
+                {
+                    var c = line[j];
+                    if (char.IsNumber(c))
+                    {
+                        numsAndIndexes.Add(j, char.GetNumericValue(c));
+                    }
+                }
+
+                var sorted = new SortedDictionary<int, double>(numsAndIndexes);
+
+                firstNum = sorted.Values.First();
+                secondNum = sorted.Values.Last();
+
+                var combined = Convert.ToInt32(firstNum.ToString() + secondNum.ToString());
+                total += combined; 
+            }
+
+            Console.WriteLine(total);
+        }
+    }
+}
