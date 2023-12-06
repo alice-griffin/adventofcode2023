e168c1ad1b5d9d137df2ec6d9280675d35616bde
alice-griffin <alicekaygriffin@gmail.com>
2023-12-06 11:15:29 -0500

    commit day 3 project

---
+++ Day3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] contents = File.ReadLines("../../TextFile1.txt").ToArray();
+
+            string previousLine = "";
+            string nextLine = "";
+
+            for(int i = 0; i < contents.Length; i+=2)
+            {
+                var line = contents[i];
+                Console.WriteLine(i);
+
+                
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
